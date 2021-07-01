4f4a159d6429a28a3fb27b3d427e05c82b67850f
Jaron-Warren <Jarwarren@gmail.com>
2021-07-01 15:38:14 -0600

    game logic

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-var health = 100
-var name = "Barney"
-var hits = 0
+let characters = {
+  Barney: {
+    health: 100,
+    name: 'Barney',
+    hits: 0,
+    weapon: []
+  }
+}
 
+let weapons = {
+  fireball: {
+    name: 'Fireball',
+    modifier: 2,
+    description: 'It burns!'
+  },
+  sword: {
+    name: 'Sword',
+    modifier: 1.5,
+    description: 'watch your head'
+  },
+  lightningBolt: {
+    name: 'Lightning Bolt',
+    modifier: 3,
+    description: 'Ouch!'
+  }
+}
 
 const attacks = {
   slap: {
@@ -15,35 +37,70 @@ const attacks = {
   }
 }
 
+let currentTarget = characters['Barney']
+
 function attack(type) {
-  hits++
+  currentTarget = characters['Barney']
+  currentTarget.hits++
   let choice = attacks[type]
   console.log(choice.damage)
-  health -= choice.damage
+  currentTarget.health = currentTarget.health - choice.damage + addMods()
 
   drawHealth()
 }
 
 function createButtons() {
-  let template = ''
+  let attacksTemplate = ''
   for (let key in attacks) {
     //let type = attacks[key]
     //console.log(type)
-    template +=
+    attacksTemplate +=
       `
       <button id="${key}-button" onclick="attack('${key}')">${key}!</button>
       `
   }
-  document.getElementById('attack-buttons').innerHTML = template
+  let weaponTemplate = ''
+  for (let key in weapons) {
+    let type = weapons[key]
+    console.log(type.name)
+    weaponTemplate +=
+      `
+    <button id="${key}-button" onclick="getWeapon('${key}')">${type.name}!</button>
+    `
+  }
+  document.getElementById('attack-buttons').innerHTML = attacksTemplate
+  document.getElementById('weapon-buttons').innerHTML = weaponTemplate
 }
 
 function drawHealth() {
   let healthElem = document.getElementById('health')
   let nameElem = document.getElementById('name')
   let hitsElem = document.getElementById('hits')
-  healthElem.innerText = health
-  nameElem.innerText = name
-  hitsElem.innerText = hits
+  healthElem.innerText = currentTarget.health.toString()
+  nameElem.innerText = currentTarget.name.toString()
+  hitsElem.innerText = currentTarget.hits.toString()
+}
+
+function getWeapon(weapon) {
+  let exists = false
+  for (let key in currentTarget.weapon) {
+    console.log(currentTarget.weapon[key])
+    if (currentTarget.weapon[key] == weapon) {
+      exists = true
+    }
+  }
+  if (!exists) {
+    currentTarget.weapon.push(weapon)
+  }
+  console.log(currentTarget.weapon)
+}
+
+function addMods() {
+  let toteMods = 0
+  currentTarget.weapon.forEach(item => {
+    toteMods += weapons[item].modifier
+  })
+  return toteMods
 }
 
 drawHealth()
